1a7bbd48605d0899d81c6144143fb7dc099a2e23
Мазурук Олег Володимирович <oleg.mazuruk@innovinnprom.com>
2024-11-17 20:32:24 +0200

    feat(JpaRepository): add spring JpaRepository for existing Jpa entities

---
+++ src/main/java/com/banking/threeom/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.banking.threeom.repository;
+
+import com.banking.threeom.domain.Transaction;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Transaction entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
